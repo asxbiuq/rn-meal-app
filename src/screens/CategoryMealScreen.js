60ed3d1493e6a9ec5197512c34260571f87fb586
17o3 <17o3gutk29vinqb@gmail.com>
2022-09-11 22:15:44 +0800

    CategoryMealScreen update

--- src/screens/CategoryMealScreen.js
+++ src/screens/CategoryMealScreen.js
@@ -1,21 +1,30 @@
-import { MEALS } from "../data/dummy-data";
 import MealList from "../components/MealList";
 import { useSelector } from "react-redux";
 import { filteredMeals } from "../slice";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 
-export default ({ navigation, route }) => {
-  // const { categoryId } = route.params
+export default ({ navigation }) => {
+  const displayedMeals = useSelector(filteredMeals)
 
-  const availableMeals = useSelector(filteredMeals)
-  // const availableMeals = useFilteredMeals()
-  // console.log(availableMeals)
-  // const displayedMeals = MEALS.filter(
-  //   meal => meal.categoryIds.indexOf(categoryId) >= 0
-  // )
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meal found, maybe check your filters?</DefaultText>
+      </View>
+    )
+  }
 
   return (
-    <MealList listData={availableMeals} navigation={navigation}/>
+    <MealList listData={displayedMeals} navigation={navigation}/>
   )
 }
 
+const styles = StyleSheet({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
